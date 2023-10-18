375d83e4e7e864a404583dae747e2aa90490cbde
manjunathhegdebalgar <hmanjub@gmail.com>
2023-10-18 12:47:42 +0530

    add tasks to d4d pipeline

--- projects/dpg_pipeline/mgnrega_flow/data_for_districts_flow.py
+++ projects/dpg_pipeline/mgnrega_flow/data_for_districts_flow.py
@@ -2,50 +2,82 @@ from prefect import task, flow
 from task_utils import *
 
 
-
 @task
 def mgnrega_scraper(context, pipeline, task_obj):
     print("inside scraper publisher!")
     data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
-    if not exception_flag:    # if there's no error while executing the task
+    if not exception_flag:  # if there's no error while executing the task
         # Replace the following with your own code if the need is different.
-        # Generally, to read the returned data into a dataframe and save it against the pipeline object for further tasks
-        #df = pd.read_csv(StringIO(data), sep=',')
         pipeline.data_path = data
         print(data)
         # Following is a mandatory line to set logs in prefect UI
         set_task_model_values(task_obj, pipeline)
         send_info_to_prefect_cloud("MGNREGA scraper task finished successfully")
     else:
-       pipeline.logger.error("ERROR: at mgnrega_scraper")
+        pipeline.logger.error("ERROR: at mgnrega_scraper")
 
 
 @task
 def mgnrega_transformer(context, pipeline, task_obj):
     data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
-    if not exception_flag:    # if there's no error while executing the task
-        # Replace the following with your own code if the need is different.
-        # Generally, to read the returned data into a dataframe and save it against the pipeline object for further tasks
-        #df = pd.read_csv(StringIO(data), sep=',')
+    if not exception_flag:  # if there's no error while executing the task
+        # df = pd.read_csv(StringIO(data), sep=',')
         pipeline.data_path = data
         # Following is a mandatory line to set logs in prefect UI
-        send_info_to_prefect_cloud("MGNREGA transforer task finished successfully! Resultant file can be found at- " + pipeline.data_path + " on the server")
+        send_info_to_prefect_cloud(
+            "MGNREGA transforer task finished successfully! Resultant file can be found at- " + pipeline.data_path + " on the server")
         set_task_model_values(task_obj, pipeline)
     else:
-       pipeline.logger.error("ERROR: at mgnrega_transformer")
+        pipeline.logger.error("ERROR: at mgnrega_transformer")
+
 
 @task
 def data_transformation_department(context, pipeline, task_obj):
     data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
-    if not exception_flag:    # if there's no error while executing the task
+    if not exception_flag:  # if there's no error while executing the task
         # Replace the following with your own code if the need is different.
-        # Generally, to read the returned data into a dataframe and save it against the pipeline object for further tasks
-        #df = pd.read_csv(StringIO(data), sep=',')
         pipeline.data_path = data
         # Following is a mandatory line to set logs in prefect UI
         set_task_model_values(task_obj, pipeline)
     else:
-       pipeline.logger.error("ERROR: at data_transformation_department")
+        pipeline.logger.error("ERROR: at data_transformation_department")
+
+
+@task
+def data_transformation_district(context, pipeline, task_obj):
+    data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
+    if not exception_flag:  # if there's no error while executing the task
+        # Replace the following with your own code if the need is different.
+        pipeline.data_path = data
+        # Following is a mandatory line to set logs in prefect UI
+        set_task_model_values(task_obj, pipeline)
+    else:
+        pipeline.logger.error("ERROR: at data_transformation_district")
+
+
+@task
+def indicator_transformation(context, pipeline, task_obj):
+    data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
+    if not exception_flag:  # if there's no error while executing the task
+        pipeline.data_path = data
+        # Following is a mandatory line to set logs in prefect UI
+        set_task_model_values(task_obj, pipeline)
+    else:
+        pipeline.logger.error("ERROR: at indicator_transformation")
+
+
+@task
+def data_transformation_explorer(context, pipeline, task_obj):
+    data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
+    if not exception_flag:  # if there's no error while executing the task
+        # Replace the following with your own code if the need is different.
+        # Generally, to read the returned data into a dataframe and save it against the pipeline object for further tasks
+        # df = pd.read_csv(StringIO(data), sep=',')
+        pipeline.data_path = data
+        # Following is a mandatory line to set logs in prefect UI
+        set_task_model_values(task_obj, pipeline)
+    else:
+        pipeline.logger.error("ERROR: at data_transformation_explorer")
 
 
 @flow
@@ -77,5 +109,3 @@ def data_for_districts_pipeline(pipeline):
     pipeline.model.output_id = str(pipeline.model.pipeline_id) + "_" + pipeline.model.status
     # print("Data after pipeline execution\n", pipeline.data)
     return
-
-
