b7224e4df8a037c7bfe06231f2280631f076204d
manjunathhegdebalgar <hmanjub@gmail.com>
2023-10-26 14:38:12 +0530

    add tenders-concat task

--- projects/IDS_DRR/ids_drr_flow.py
+++ projects/IDS_DRR/ids_drr_flow.py
@@ -2,6 +2,19 @@ from prefect import task, flow
 from task_utils import *
 
 
+@task
+def tenders_concat_tenders(context, pipeline, task_obj):
+    data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
+    if not exception_flag:  # if there's no error while executing the task
+        # Replace the following with your own code if the need is different.
+        # Generally, to read the returned data into a dataframe and save it against the pipeline object for further tasks
+        # df = pd.read_csv(StringIO(data), sep=',')
+        pipeline.data_path = data
+        # Following is a mandatory line to set logs in prefect UI
+        set_task_model_values(task_obj, pipeline)
+    else:
+        pipeline.logger.error("ERROR: at tenders_concat_tenders")
+
 
 @task
 def tenders_scrape_data(context, pipeline, task_obj):
@@ -15,15 +28,15 @@ def tenders_scrape_data(context, pipeline, task_obj):
     Output: Creates folders in the server. Returns nothing.
     """
     data, exception_flag = publish_task_and_process_result(task_obj, context, pipeline.data_path)
-    if not exception_flag:    # if there's no error while executing the task
+    if not exception_flag:  # if there's no error while executing the task
         # Replace the following with your own code if the need is different.
         # Generally, to read the returned data into a dataframe and save it against the pipeline object for further tasks
-        #df = pd.read_csv(StringIO(data), sep=',')
+        # df = pd.read_csv(StringIO(data), sep=',')
         pipeline.data_path = data
         # Following is a mandatory line to set logs in prefect UI
         set_task_model_values(task_obj, pipeline)
     else:
-       pipeline.logger.error("ERROR: at tenders_scrape_data")
+        pipeline.logger.error("ERROR: at tenders_scrape_data")
 
 
 @task
